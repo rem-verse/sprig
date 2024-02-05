409f0aca813e197904cf1fb5c3b5a1228e9c8245
Cynthia <cynthia@coan.dev>
2024-02-05 05:46:14 +0000

    update to version 0.0.2 everywhere

--- installer-scripts/osx/package.sh
+++ installer-scripts/osx/package.sh
@@ -15,5 +15,5 @@ cp ../../../cmd/getbridge/sh/getbridge ./
 cp ../../../cmd/getbridgetype/sh/getbridgetype ./
 cp ../../../cmd/setbridge/sh/setbridge ./
 cd ../
-pkgbuild --root ./working-dir/ --identifier "dev.rem-verse.sprig" --version "0.0.1" --install-location "/usr/local/bin" sprig.pkg
+pkgbuild --root ./working-dir/ --identifier "dev.rem-verse.sprig" --version "0.0.2" --install-location "/usr/local/bin" sprig.pkg
 productbuild --synthesize --package "sprig.pkg" sprig.dist
