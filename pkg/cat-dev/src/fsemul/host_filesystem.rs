ca956571d10e72a40ee0050faf65c56b1d03dcff
Cynthia <cynthia@coan.dev>
2025-01-14 05:55:35 +0000

    properly check for parent directory, canonicalize for mac

--- pkg/cat-dev/src/fsemul/host_filesystem.rs
+++ pkg/cat-dev/src/fsemul/host_filesystem.rs
@@ -519,7 +519,8 @@ impl HostFilesystem {
 			return Err(PCFSApiError::PathNotMapped(path.to_owned()).into());
 		}
 		// Check for mapped directories...
-		if !closest_canonical_directory.starts_with(self.cafe_sdk_path()) {
+		let canonicalized_cafe = self.cafe_sdk_path().canonicalize().unwrap_or_else(|_| self.cafe_sdk_path().clone());
+		if !closest_canonical_directory.starts_with(canonicalized_cafe) {
 			return Err(PCFSApiError::PathNotMapped(path.to_owned()).into());
 		}
 
